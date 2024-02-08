8e95be0f2696c3ccdb7a6582588d09f113c0c1ac
DarioGuastella <jimmyguastella@gmail.com>
2024-02-08 16:34:22 +0100

    usa props "id"

--- src/pages/EventDetail.vue
+++ src/pages/EventDetail.vue
@@ -8,7 +8,7 @@ export default {
 <template>
     <div class="container">
         <div class="row">
-            <h1 class="mb-3">Dettaglio evento {{ this.$route.params.id }}</h1>
+            <h1 class="mb-3">Dettaglio evento {{ id }}</h1>
             <p>Da qui posso leggere solo l'id.</p>
             <p>Oppure...</p>
         </div>
