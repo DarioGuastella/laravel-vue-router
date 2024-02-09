356a00f3890920840f1822fc4f1d73480a66d874
DarioGuastella <jimmyguastella@gmail.com>
2024-02-09 12:27:47 +0100

    refactoring lettura dati store

--- src/pages/EventDetail.vue
+++ src/pages/EventDetail.vue
@@ -5,8 +5,12 @@ export default {
     props: ["id"],
     data() {
         return {
-            store
+            store,
+            event: null
         }
+    },
+    mounted() {
+        this.event = this.store.eventList.find(item => item.id == this.id);
     }
 }
 </script>
@@ -14,11 +18,11 @@ export default {
 <template>
     <div class="container">
         <div class="row">
-            <h1 class="mb-3">Nome evento: {{ store.eventList[id - 1].name }}</h1>
-            <h3 class="mb-3">A cura di: {{ store.eventList[id - 1].user.name }}</h3>
-            <h5 class="mb-3"> Email: {{ store.eventList[id - 1].user.email }}</h5>
-            <h3 class="mb-3">Data dell'evento: {{ store.eventList[id - 1].date }}</h3>
-            <h3 class="mb-3">Biglietti rimanenti: {{ store.eventList[id - 1].available_tickets }}</h3>
+            <h1 class="mb-3">Nome evento: {{ event?.name }}</h1>
+            <h3 class="mb-3">A cura di: {{ event?.user.name }}</h3>
+            <h5 class="mb-3"> Email: {{ event?.user.email }}</h5>
+            <h3 class="mb-3">Data dell'evento: {{ event?.date }}</h3>
+            <h3 class="mb-3">Biglietti rimanenti: {{ event?.available_tickets }}</h3>
             <router-link :to="{ name: 'Events' }" class="btn btn-primary">
                 <span>Torna alla lista eventi</span>
             </router-link>
