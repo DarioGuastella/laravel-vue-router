2827e10537b3cd58891576decd609574dfdfdcd9
DarioGuastella <jimmyguastella@gmail.com>
2024-02-09 13:02:51 +0100

    stampa tags in dettaglio

--- src/pages/EventDetail.vue
+++ src/pages/EventDetail.vue
@@ -1,5 +1,6 @@
 <script>
 import { store } from "../store.js"
+import axios from "axios";
 export default {
     name: "EventDetail",
     props: ["id"],
@@ -10,7 +11,34 @@ export default {
         }
     },
     mounted() {
-        this.event = this.store.eventList.find(item => item.id == this.id);
+        // this.event = this.store.eventList.find(item => item.id == this.id);
+        this.getEventDetail();
+    },
+    methods: {
+        getEventDetail() {
+            let url = this.store.apiUrl + this.store.apiEventEndpoint + this.id;
+
+            axios.get(url).then(result => {
+                if (result.status === 200) {
+                    if (result.data.success) {
+                        this.event = result.data.payload;
+                    } else {
+                        console.error("Ops... non siamo in grado di soddisfare la richiesta.");
+                    }
+
+                } else if (result.status === 301) {
+                    console.error("Ops... ciò che cerchi non si trova più qui.");
+                } else if (result.status === 400) {
+                    console.error("Ops... non riusciamo a comprendere ciò che hai richiesto.");
+                } else if (result.status === 404) {
+                    console.error("Ops... non riusciamo a trovare ciò che hai richiesto.");
+                } else if (result.status === 500) {
+                    console.error("Ops... ci scusiamo per l'inconveniente, stiamo spegnendo l'incendio.");
+                }
+            }).catch(errore => {
+                console.error(errore);
+            });
+        }
     }
 }
 </script>
@@ -22,6 +50,7 @@ export default {
             <h3 class="mb-3">A cura di: {{ event?.user.name }}</h3>
             <h5 class="mb-3"> Email: {{ event?.user.email }}</h5>
             <h3 class="mb-3">Data dell'evento: {{ event?.date }}</h3>
+            <h3 class="mb-3">Tag associati: <span class="mx-2" v-for="tag in event?.tags">{{ tag.name }}</span></h3>
             <h3 class="mb-3">Biglietti rimanenti: {{ event?.available_tickets }}</h3>
             <router-link :to="{ name: 'Events' }" class="btn btn-primary">
                 <span>Torna alla lista eventi</span>
